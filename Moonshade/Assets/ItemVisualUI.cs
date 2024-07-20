f16b2e246e6fc74b9c586484d02f3e94509d0d90
Malii61 <92629282+Malii61@users.noreply.github.com>
2024-07-20 20:20:00 +0300

    güncel itemin sol altta görünmesi

---
+++ Moonshade/Assets/ItemVisualUI.cs
@@ -0,0 +1,27 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemVisualUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private Image itemImage;
+    public static ItemVisualUI Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateVisual(ItemManager.LocalInstance.GetCurrentItem());
+    }
+
+    public void UpdateVisual(Item item)
+    {
+        itemName.text = item.itemName;
+        itemImage.sprite = item.itemSprite;
+    }
+}
