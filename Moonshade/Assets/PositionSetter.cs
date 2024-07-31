aaf32e4bb47136f590f23b9818369d3f202dc9dc
Malii61 <92629282+Malii61@users.noreply.github.com>
2024-07-31 19:18:08 +0300

    Update PositionSetter.cs

--- Moonshade/Assets/PositionSetter.cs
+++ Moonshade/Assets/PositionSetter.cs
@@ -5,11 +5,13 @@ public class PositionSetter : MonoBehaviour
 {
     [SerializeField] private Vector3 spawnPos;
     [SerializeField] private Camera cam;
+
     void Start()
     {
         spawnPos = new Vector3(spawnPos.x + Random.Range(-0.5f, 0.5f), spawnPos.y,
             spawnPos.z + Random.Range(-0.5f, 0.5f));
         PlayerController.LocalInstance.SetPosition(spawnPos);
         cam.gameObject.SetActive(false);
+        Cursor.visible = false;
     }
 }
